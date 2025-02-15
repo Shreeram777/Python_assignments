65424141749db02eaa8c10d522e274e866b1e5ba
Shreeram777 <shreeramrg77@gmail.com>
2025-02-15 12:05:28 +0530

    Add files via upload

---
+++ Write a program to illustrate the if condition, if 'A' in entered it display the word 'Apricots'; if 'B' is entered, it displays 'Blueberries'; and if 'C' is entered, it displays 'Cherries'. Use nested if statement.py
@@ -0,0 +1,18 @@
+hello = input("Enter a letter (A, B,C or D,code): ")
+
+if hello == 'A':
+    print("Apricots")
+else:
+    if hello == 'B':
+        print("Blueberries")
+    else:
+        if hello == 'C':
+            print("Cherries")
+        else:
+            if hello == "D":
+                print ("Nirmal kumar")
+            else:
+                if hello =="code":
+                    print ("10 maniku bomb vedikum da mapla varata")
+                else:
+                    print("Invalid input. Please enter A, B, or C.")
